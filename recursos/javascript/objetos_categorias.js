9b86907ea2382c63f3e11ddc781aa3623a7312ff
santyzenith <santyg20@gmail.com>
2020-12-14 17:54:50 -0500

    añadir las oraciones

---
+++ recursos/javascript/objetos_categorias.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+
+//let categoria = document.getElementsByClassName("categoria-item");
+//let clon;
+
+//for (let i = 0; i < categoria.length; i++) {
+//    categoria[i].addEventListener('click', () => {
+//        clon = categoria[i].importNode(true);
+//        p = document.createElement("P");
+//        p.innerHTML = "hola q tal";
+//    });
+
+//}
+
+//let body = document.getElementById('body-objetos');
+//body.appendChild(p);
+//let p = document.createElement("P");
+//p.innerHTML = "Hola que mas";
+//body.appendChild(p);
+//body.appendChild(clon);
